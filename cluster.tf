d77d1ec1e1a5309ad6df8c4a7962eb97b8dc31f0
Mani M <maannniii@gmail.com>
2019-11-30 12:25:31 +0530

    added kubernetes credentials variable and created cluster

--- cluster.tf
+++ cluster.tf
@@ -10,6 +10,16 @@ variable "credentials" {
   description = "Google Cloud Service Account"
   default = "account.json"
 }
+variable "k8s_username" {
+  description = "Kubernetes username"
+  type = string
+  default = ""
+}
+variable "k8s_password" {
+  description = "Kubernetes password"
+  type = string
+  default = ""
+}
 
 provider "google" {
   project = var.project
@@ -25,5 +35,54 @@ resource "google_compute_network" "counter-network" {
     ignore_changes = []
     create_before_destroy = false
     prevent_destroy = false
+  }
+}
+
+resource "google_container_cluster" "primary" {
+  name = "counter"
+  location = var.region
+  remove_default_node_pool = true
+  initial_node_count = 1
+
+  master_auth {
+    username = var.k8s_username
+    password = var.k8s_password
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+  addons_config {
+    horizontal_pod_autoscaling {
+      disabled = false
+    }
+    http_load_balancing {
+      disabled = false
+    }
+  }
+  ip_allocation_policy {
+
+  }
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name = "nodepool"
+  location = var.region
+  cluster = google_container_cluster.primary.name
+  node_count = 1
+  management {
+    auto_repair = true
+    auto_upgrade = true
+  }
+  node_config {
+    preemptible = true
+    machine_type = "custom-1-1024"
+    disk_size_gb = 10
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
   }
 }
