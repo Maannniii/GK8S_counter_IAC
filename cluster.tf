cdc4d7ae40720195e5a0107cf57c8aa1659dbdb6
Mani M <maannniii@gmail.com>
2019-11-30 11:45:05 +0530

    added variables

--- cluster.tf
+++ cluster.tf
@@ -3,3 +3,16 @@ provider "google" {
   region = "us-central1"
   credentials ="account.json"
 }
+
+variable "project" {
+  description = "Google Cloud project ID"
+  default = "projectid"
+}
+variable "region" {
+  description = "Google Cluster Region"
+  default = "asia-southeast1"
+}
+variable "credentials" {
+  description = "Google Cloud Service Account"
+  default = "account.json"
+}
