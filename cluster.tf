7a9d826f3253e6e4800ccdab4d48c6fb4bc17a99
Mani M <maannniii@gmail.com>
2019-11-30 14:19:34 +0530

    added k8s deployment and load balancer service

--- cluster.tf
+++ cluster.tf
@@ -69,7 +69,7 @@ rresource "google_container_cluster" "primary" {
       "bash",
       "-c"]
   }
-  
+
   network = google_compute_network.counter-network.self_link
 }
 
@@ -128,5 +128,87 @@ resource "kubernetes_config_map" "redis_url" {
       ":",
       google_redis_instance.cache.port,
       "/0"])
+  }
+}
+
+resource "kubernetes_deployment" "counter" {
+  metadata {
+    name = "counter"
+    namespace = kubernetes_namespace.namespace.metadata[0].name
+  }
+
+  spec {
+    replicas = 3
+    strategy {
+      rolling_update {
+        max_surge = "1"
+        max_unavailable = "1"
+      }
+    }
+    selector {
+      match_labels = {
+        app = "UI"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "UI"
+        }
+      }
+
+      spec {
+        container {
+          image = "tarunbhardwaj/flask-counter-app"
+          name = "flask-counter-app"
+          port {
+            container_port = "5000"
+          }
+          env_from {
+            config_map_ref {
+              name = kubernetes_config_map.redis_url.metadata[0].name
+            }
+          }
+          resources {
+            limits {
+              cpu = "0.5"
+              memory = "512Mi"
+            }
+            requests {
+              cpu = "250m"
+              memory = "50Mi"
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/"
+              port = 5000
+            }
+
+            initial_delay_seconds = 10
+            period_seconds = 10
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "frontendservice" {
+  metadata {
+    name = "frontend"
+    namespace = kubernetes_namespace.namespace.metadata[0].name
+  }
+  spec {
+    type = "LoadBalancer"
+    port {
+      port = 80
+      target_port = kubernetes_deployment.counter.spec[0].template[0].spec[0].container[0].port[0].container_port
+    }
+    selector = {
+      app = "UI"
+    }
   }
 }
