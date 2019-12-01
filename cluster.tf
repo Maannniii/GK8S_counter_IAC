0d7bc5db135661ff1399035a9d05fbef5ee98311
Mani M <maannniii@gmail.com>
2019-12-01 12:09:23 +0530

    added commnets

--- cluster.tf
+++ cluster.tf
@@ -24,10 +24,13 @@ resource "google_container_cluster" "primary" {
   master_auth {
     username = var.k8s_username
     password = var.k8s_password
+    # don't display certificates on terraform apply
     client_certificate_config {
       issue_client_certificate = false
     }
   }
+
+  # enable pod auto scaling and load balancing by default enabled
   addons_config {
     horizontal_pod_autoscaling {
       disabled = false
@@ -39,19 +42,24 @@ resource "google_container_cluster" "primary" {
   ip_allocation_policy {
 
   }
+
+  # create kube config file when the cluster is created
   provisioner "local-exec" {
     command = "gcloud container clusters get-credentials ${self.name} --region ${self.location} --project ${var.project}"
     interpreter = [
       "bash",
       "-c"]
   }
+  # network to be attached to
   network = google_compute_network.counter-network.self_link
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
   name = "nodepool"
+  # This is a regional cluster in order to make sure fault tolerance
   location = var.region
   cluster = google_container_cluster.primary.name
+  # no of nodes be created in each zone
   node_count = 1
   management {
     auto_repair = true
@@ -69,6 +77,11 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
       "https://www.googleapis.com/auth/monitoring",
     ]
   }
+
+  //  autoscaling {
+  //    max_node_count = 9
+  //    min_node_count = 3
+  //  }
 }
 
 resource "google_redis_instance" "cache" {
